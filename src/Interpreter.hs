d144b69658f9a8ba22ba11f7c3ff4ae4b4eedb4a
Patrick Scheidegger <patrick.scheidegger@ost.ch>
2022-05-16 07:22:03 +0200

    alpha conversion added

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -8,12 +8,12 @@ data Term
   = Var Id -- Variables
   | Abs Id Term -- Abstractions
   | App Term Term -- Applications
-  deriving (Eq, Show)
+  deriving (Eq)
 
--- instance Show Term where
---   show (Var varName) = varName
---   show (Abs varName term) = "(%" ++ varName ++ ". " ++ show term ++ ")"
---   show (App term1 term2) = show term1 ++ " " ++ show term2
+instance Show Term where
+  show (Var varName) = varName
+  show (Abs varName term) = "(%" ++ varName ++ ". " ++ show term ++ ")"
+  show (App term1 term2) = show term1 ++ " " ++ show term2
 
 -- ----- Test Values -----
 testLambda1 = App (Abs "x" (Abs "y" (App (Var "x") (Var "y")))) (Var "y")
@@ -39,11 +39,16 @@ testLambda7 = App (App (Abs "z" (Var "z")) (Abs "q" (App (Var "q") (Var "q")))) 
 -- >>> testLambda7
 -- (%z. z) (%q. q q) (%s. s a)
 
+-- (λx.λy. y) y
+testLambda8 = App (Abs "x" (Abs "y" (Var "y"))) (Var "y")
+-- >>> testLambda8
+-- (%x. (%y. y)) y
 
-
+testLambda9 = App (Abs "x" (Abs "y1" (Var "y"))) (Var "y")
+-- >>> testLambda9
+-- (%x. (%y1. y)) y
 
 -- ----- END -----
-
 
 -- ----- Example Usage -----
 -- `(λx.x) a` reduces to `a`
@@ -59,7 +64,7 @@ testLambda7 = App (App (Abs "z" (Var "z")) (Abs "q" (App (Var "q") (Var "q")))) 
 -- You may want to represent sets as lists without duplicates.
 -- Appendix A.2 of the lecture notes contains recursive definitions of non-freeness that you may find useful.
 freeVars :: Term -> [Id]
-freeVars term = freeVarsImpl term []
+freeVars term = nub (freeVarsImpl term [])
 
 -- Implementation. Done in order to avoid passing an empty array on each invocation
 freeVarsImpl :: Term -> [Id] -> [Id]
@@ -84,6 +89,8 @@ boundVarsImpl (App term1 term2) = boundVarsImpl term1 ++ boundVarsImpl term2
 -- In case you find the task of avoiding variable capture too challenging, skip this and only use terms with
 -- unique bound and free variable names
 -- substitute (x,tx) t = undefined
+
+-- !!! LEGACY CODE, PLEASE SKIP !!!
 substitute :: (Term, Term) -> Term -> Term
 substitute (Var toReplace, Var newValue) term =
   if isSubOldVar toReplace term && isSubNewVar newValue term then substituteImpl (Var toReplace, Var newValue) term else term
@@ -135,8 +142,8 @@ isBetaRedex :: Term -> Bool
 -- isBetaRedex (Abs varName term) = False -- E.g. (\x. a b) cannot be reduced
 -- isBetaRedex (App term1 term2) = True -- May be wrong, e.g. "a (λx. x)" cannot be reduced further
 isBetaRedex (App (App _ _) _) = True
-isBetaRedex (App (Abs _ _) _) = True 
-isBetaRedex (App _ (App _ _)) = True 
+isBetaRedex (App (Abs _ _) _) = True
+isBetaRedex (App _ (App _ _)) = True
 isBetaRedex _ = False
 
 
@@ -150,7 +157,7 @@ betaReduce :: Term -> Term
 -- betaReduce (Abs varName term) = Abs varName (betaReduce term)
 betaReduce (App (Abs y term1) term2) = substitute2 (y, term2) term1
 --betaReduce (App term2 (Abs y term1)) = substitute2 (y, term2) term1
-betaReduce (App term1 term2) 
+betaReduce (App term1 term2)
   | isBetaRedex term1 = App (betaReduce term1) term2
   | isBetaRedex term2 = App term1 (betaReduce term2)
   | otherwise = App term1 term2
@@ -179,10 +186,16 @@ betaReduce term = term
 -- >>> reduce testLambda7
 -- App (Var "a") (Var "a")
 
+-- >>> reduce testLambda8
+-- (%y1. y1)
+
+-- >>> reduce testLambda9
+-- (%y1. y)
+
 reduce :: Term -> Term
-reduce term -- = betaReduce (betaReduce term) 
-  | isBetaRedex term = reduce (betaReduce term) --TODO uncomment me
-  | otherwise = term 
+reduce term
+  | isBetaRedex term = reduce (betaReduce term)
+  | otherwise = term
 
 -- >>> testLambda1
 -- (%x. (%y. x y)) y
@@ -194,7 +207,7 @@ reduce term -- = betaReduce (betaReduce term)
 -- returns true if the given var is not used a free var in the given term
 isNonFree :: Id -> Term -> Bool
 isNonFree toCheck (Var varName) = toCheck /= varName
-isNonFree toCheck (Abs varName term) 
+isNonFree toCheck (Abs varName term)
   | toCheck == varName = True
   | otherwise = isNonFree toCheck term
 isNonFree toCheck (App term1 term2) = isNonFree toCheck term1 && isNonFree toCheck term2
@@ -211,5 +224,32 @@ substitute2 (x, termL) (Var y)
 substitute2 (x, termL) (Abs y termM)
   | x == y = substitute2 (x, termL) termM --TODO change me to "Abs y termM" if it still does not work
   | isNonFree y termL = Abs y (substitute2 (x, termL) termM)
-  | otherwise = undefined --TODO here is some alpha conversion needed
+  | otherwise = substitute2 (x, termL) (alphaRename (y, getUnusedId y (freeVars termL)) (Abs y termM)) -- alpha conversion
 substitute2 (x, termL) (App termM termN) = App (substitute2 (x, termL) termM) (substitute2 (x, termL) termN)
+
+-- >>> getUnusedId "x" ["x", "x1", "x11"]
+-- "x111"
+
+-- >>> getUnusedId "y" ["x", "x1", "x11"]
+-- "y"
+
+-- >>> getUnusedId "z" []
+-- "z"
+
+getUnusedId :: Id -> [Id] -> Id
+getUnusedId toCheck freeVars
+  | toCheck `elem` freeVars = getUnusedId (toCheck ++ "1") freeVars
+  | otherwise = toCheck
+
+-- >>> alphaRename ("z", "z1") testLambda7
+-- (%z1. z1) (%q. q q) (%s. s a)
+
+
+alphaRename :: (Id, Id) -> Term -> Term
+alphaRename (toReplace, new) (Var x)
+    | toReplace == x = Var new
+    | otherwise = Var x
+alphaRename (toReplace, new) (Abs x term)
+    | toReplace == x = Abs new (alphaRename (toReplace, new) term)
+    | otherwise = Abs x (alphaRename (toReplace, new) term)
+alphaRename (toReplace, new) (App t1 t2) = App (alphaRename (toReplace, new) t1) (alphaRename (toReplace, new) t2)
